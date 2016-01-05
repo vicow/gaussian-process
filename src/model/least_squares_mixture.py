e13da662339d50b76e10f2cc392bd3079a35e3fb
Victor Kristof <victor.kristof@epfl.ch>
2016-01-05 17:25:49 +0100

    Now predicting using euclidean distance with training set and implemented evaluation method.

--- src/model/least_squares_mixture.py
+++ src/model/least_squares_mixture.py
@@ -5,15 +5,25 @@ import scipy.linalg as lin
 
 from .utils import ProgressBar
 from .model import Model, ModelError
+from collections import Counter
 
 
 class LeastSquaresMixture(Model):
 
     # List of allowed keys for use with kwargs
-    allowed_keys = ['K', 'beta', 'lam', 'iterations', 'epsilon', 'random_restarts', 'verbose']
+    allowed_keys = ['K', 'beta', 'lam', 'iterations', 'epsilon', 'random_restarts']
 
     def __init__(self, X, y, K=2, beta=1, lam=0, iterations=1000, epsilon=1e-4, random_restarts=None, verbose=False):
+        """
+        :param K:           Number of mixture components
+        :param beta:        Precision term for the probability of the data under the regression function
+        :param lam:         Regularization parameter for the regression weights
+        :param iterations:  Maximum number of iterations
+        :param epsilon:     Condition for convergence
+        :param verbose:     Display some information in the console
+        """
         super(LeastSquaresMixture, self).__init__(self.__class__.__name__ + " (%s components)" % K, X, y)
+
         # Model hyperparameters
         self.K = K
         self.lam = lam
@@ -24,13 +34,11 @@ class LeastSquaresMixture(Model):
         N, D = X.shape
         self.w = np.zeros((D, K))
         self.pi = np.zeros(K)
+        self.gamma = np.zeros((N, K))
         self.beta = beta
         self.marginal_likelihood = - np.inf
         self.trained = False
         self.random_restarts = random_restarts
-
-        # Other
-        self.verbose = verbose
 
     def __str__(self):
         description = "Model: %s\n" % self.name
@@ -40,31 +48,26 @@ class LeastSquaresMixture(Model):
             description += "Lambda: %s\n" % self.lam
             description += "Pi: %s\n" % self.pi
             description += "Weights: (norm: %s)\n" % [np.linalg.norm(self.w[:, i]) for i in range(self.K)]
-            description += "%s" % self.w
+            if self.w.shape[0] <= 20:
+                description += "%s" % self.w
         else:
             description += "Not trained yet"
         return description
 
-    def _expectation_maximization(self):
+    def _expectation_maximization(self, verbose=False):
         """
-        Compute the weights for a mixture of least squares.
+        Learn the parameters for a mixture of least squares.
         Source:
         - http://stats.stackexchange.com/questions/33078/data-has-two-trends-how-to-extract-independent-trendlines/34287
 
-        :param K:           Number of mixture components
-        :param beta:        Precision term for the probability of the data under the regression function
-        :param lam:         Regularization parameter for the regression weights
-        :param iterations:  Maximum number of iterations
-        :param epsilon:     Condition for convergence
-        :param verbose:     Display some information in the console
-        :return:            Weights vectors for each line, pi_k's and beta parameter
+        :return: Weights vectors, pi_k's, gamma's, beta and marginal likelihood
         """
 
         # Get the dimensions
         N = self.X.shape[0]
         D = self.X.shape[1] + 1  # + 1 to take bias into account
 
-        if self.verbose:
+        if verbose:
             print("* Starting EM algorithm for mixture of K=%s least squares models" % self.K)
             print("* Beta = %s" % self.beta)
             print("* Lambda = %s" % self.lam)
@@ -90,7 +93,7 @@ class LeastSquaresMixture(Model):
         complete_log_likelihood = - np.inf
         complete_log_likelihood_old = - np.inf
 
-        if self.verbose:
+        if verbose:
             print("Obj\t\tpi1\t\tpi2\t\tw11\t\tw12\t\tw21\t\tw22\t\tbeta")
 
         for i in range(self.iterations):
@@ -127,7 +130,7 @@ class LeastSquaresMixture(Model):
             # Evaluate the complete data log-likelihood to test for convergence
             complete_log_likelihood = float(np.sum(np.log(np.sum(np.tile(pi, (N, 1)) * probabilities, axis=1))))
 
-            if self.verbose:
+            if verbose:
                 print("%0.2f\t%0.2f\t%0.2f\t%0.2f\t%0.2f\t%0.2f\t%0.2f\t%0.2f" % (complete_log_likelihood,
                                                                                   pi[0], pi[1],
                                                                                   w[0, 0], w[1, 0],
@@ -136,57 +139,106 @@ class LeastSquaresMixture(Model):
             try:
                 if np.isnan(complete_log_likelihood) \
                         or np.abs(complete_log_likelihood - complete_log_likelihood_old) < self.epsilon:
-                    return w, pi, beta, complete_log_likelihood
+                    return w, pi, gamma, beta, complete_log_likelihood
             except:
                 pass
 
             complete_log_likelihood_old = complete_log_likelihood
 
         print("Hitting maximum iteration (%s)" % self.iterations)
-        return w, pi, beta, complete_log_likelihood
+        return w, pi, gamma, beta, complete_log_likelihood
 
-    # def posterior(self, x_new, y_new, k):
-    #     tx = np.array([1, x_new])
-    #     w_k = self.w[:, k]
-    #     pi_k = self.pi[k]
-    #     return - pi_k * 0.5 * (np.log(2 * np.pi) - np.log(self.beta) + (y_new - np.dot(w_k, tx))**2 * self.beta)
+    def train(self, seed=None, verbose=False, silent=False, **kwargs):
+        """
+        Train a mixture of least squares.
 
-    def train(self, seed=None, **kwargs):
+        :param seed:    Set the seed to fix the randomness
+        :param verbose: Display details during expectation maximization
+        :param silent:  Force to display nothing (a progress bar is displayed with random restarts even if non verbose)
+        :param kwargs:  Set the hyperparameters if needed
+        """
         self.__dict__.update((k, v) for k, v in kwargs.items() if k in self.allowed_keys)
         if seed:
             np.random.seed(seed)
         if self.random_restarts and self.random_restarts > 0:
-            bar = ProgressBar(self.random_restarts, count=True, text="Random restarts")
-            w = pi = b = None
+            w = pi = gamma = b = None
             marginal_likelihood = - np.inf
-            bar.start()
+            if not verbose and not silent:
+                bar = ProgressBar(self.random_restarts, count=True, text="Random restarts")
+                bar.start()
             for r in range(self.random_restarts):
-                w_new, pi_new, b_new, marginal_likelihood_new = self._expectation_maximization()
+                w_new, pi_new, gamma_new, b_new, marginal_likelihood_new = self._expectation_maximization(verbose=verbose)
                 if marginal_likelihood_new > self.marginal_likelihood:
                     # print("Improved solution!")
                     w = w_new
                     pi = pi_new
+                    gamma = gamma_new
                     b = b_new
                     marginal_likelihood = marginal_likelihood_new
-                bar.update(r)
+                if not verbose and not silent:
+                    bar.update(r)
             self.w = w
             self.pi = pi
+            self.gamma = gamma
             self.beta = b
             self.marginal_likelihood = marginal_likelihood
         else:
-            self.w, self.pi, self.beta, self.marginal_likelihood = self._expectation_maximization()
+            self.w, self.pi, self.gamma, self.beta, self.marginal_likelihood = self._expectation_maximization(verbose=verbose)
         self.trained = True
+
+    def _compute_euclidean_distances(self, x):
+        distances = []
+        for x_i in self.X:
+            distances.append(np.linalg.norm(x_i - x))
+        return distances
+
+    def _get_closest_point(self, x_new):
+        distances = self._compute_euclidean_distances(x_new)
+        return np.argmin(distances)
 
     def predict(self, x_new):
         if self.trained:
-            if type(x_new) is list:
-                tx = np.ones((1, len(x_new) + 1))
+            x_new = list(x_new)
+            if len(x_new) == self.X.shape[1]:
+                n = self._get_closest_point(x_new)
+                k = np.argmax(self.gamma[n, :])
+                w_k = self.w[:, k]
+                tx = np.ones((1, len(x_new )+ 1))
                 tx[0, 1:] = x_new
-                y_new = np.dot(tx, self.w)[0]
+                # print("Candidates: %s" % np.dot(tx, self.w))
+                # print("Gamma     : %s" % self.gamma[n, :])
+                # print("Chosen    : %s" % k)
+                y_new = np.dot(tx, w_k)[0]
+                return y_new, k
             else:
-                tx = np.array([1, x_new])
-                y_new = np.dot(tx, self.w)
-            posteriors = []
-            return y_new, posteriors
+                raise(ModelError("Invalid size for new data point (%s instead of %s)" % (len(x_new), self.X.shape[1])))
         else:
             raise(ModelError("Model not trained"))
+
+    def evaluate(self, X_test, y_test, verbose=False):
+        if verbose:
+            print("Evaluating model %s..." % self.name)
+        se = 0
+        accurate = 0
+        chosen = Counter()
+        bar = ProgressBar(end_value=X_test.shape[0], text="Data point", count=True)
+        bar.start()
+        for i, x_new in enumerate(X_test):
+            y_actual = y_test[i][0]
+            y_new, k = self.predict(list(x_new))
+            # print("Predicted: %s | Actual: %s" % (y_new, y_actual))
+            chosen.update([k])
+            se += (y_actual - y_new)**2
+            if (y_new >= 1 and y_actual >= 1) or (y_new < 1 and y_actual < 1):
+                accurate += 1
+            bar.update(i)
+
+        rmse = np.sqrt(np.mean(se))
+        accuracy = accurate / float(y_test.size)
+
+        if verbose:
+            print("Accuracy: %s" % accuracy)
+            print("RMSE    : %s" % rmse)
+            print("Chosen  : %s" % chosen)
+
+        return rmse, accuracy, chosen
