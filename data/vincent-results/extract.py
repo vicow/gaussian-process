22103f6934e4ad79de0073fa3b762449c92dd39b
Victor Kristof <victor.kristof@epfl.ch>
2016-03-31 09:46:17 +0200

    Extracting reduced data set results for Sidekick data

--- data/vincent-results/extract.py
+++ data/vincent-results/extract.py
@@ -9,15 +9,16 @@ std = []
 for p in np.linspace(2.5, 100, 40):
     if p == 100.0:
         break
-    file_name = "results_C=100.0_action=predict_combiner=svm_gamma=0.1_n=10_nobaseline=False_p=%s_paper=False_proba=True_which=all.txt" % p
+    file_name = "../vincent/reduced_dataset/results_C=100.0_action=predict_combiner=svm_dosave=False_gamma=0.1_n=10_nobaseline=False_p=%s_paper=False_proba=True_talk=False_which=all.txt" % p
+    #file_name = "results_C=100.0_action=predict_combiner=svm_gamma=0.1_n=10_nobaseline=False_p=%s_paper=False_proba=True_which=all.txt" % p
     with open(file_name, "r") as f:
         s = f.read()
         d = json.loads(s)
         accuracy.append(d["median"])
         std.append(math.sqrt(d["variance"]))
 
-with open("vincent_accuracy_median.pkl", 'wb') as f:
+with open("vincent_reduced_dataset_accuracy_median.pkl", 'wb') as f:
     cp.dump(accuracy, f)
 
-with open("vincent_accuracy_std.pkl", 'wb') as f:
+with open("vincent_reduced_dataset_accuracy_std.pkl", 'wb') as f:
     cp.dump(std, f)
